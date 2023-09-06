8f05bda484f1f00d77fe18dc247bcb0711f82dba
Yunhan Qiao <89273370+yqiao18@users.noreply.github.com>
2023-09-05 22:03:00 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling effect
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate(
+        {
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+    );
+});
